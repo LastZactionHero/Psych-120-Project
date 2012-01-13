a33630ea5716e8fc997f2b3b4e82cf49c9564eee
Zach Dicklin <zdicklin@gmail.com>
2012-01-12 19:05:12 -0700

    Commenting

--- app/controllers/admin/responses_controller.rb
+++ app/controllers/admin/responses_controller.rb
@@ -1,9 +1,12 @@
 class Admin::ResponsesController < Admin::AdminController
   
+  # Dump all tests, sorted by week and user
   def dump    
     @tests = Test.order( 'week ASC' ).order( 'study_user_id ASC' )  
   end
   
+  # Delete all responses
+  # Reset all tests to their original condition  
   def clear_all
     Test.all.each do |test|
       test.clear_all
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,15 @@
 class Test < ActiveRecord::Base
   belongs_to :study_user
   
+  # Get all responses, sorted by created date
   def responses
     Response.where( :test_id => self.id,  ).order( 'created_at ASC' )
   end
   
+  # Generate counterbalance conditions for all tests for a user
+  # max_weeks: Depreciated, current experiment has only two weeks
+  # Week 1: Either study or study/recall
+  # Week 2: Exam
   def self.generate_counterbalance_conditions_for_user( user, max_weeks )
     conditions = Array.new
     
@@ -19,38 +24,34 @@ class Test < ActiveRecord::Base
     conversion[symbol]
   end
   
+  # Get the test condition
+  # None, Study, Study/Recall, Exam
   def get_condition
-    if self.condition == "none"
-      condition = :none        
-    elsif self.condition == "study"
-      condition = :study      
-    elsif self.condition == "study_recall"    
-      condition = :study_recall
-    else
-      condition = :exam    
-    end
-    condition  
+    conversion = { "none" => :none, "study" => :study, "study_recall" => :study_recall, "exam" => :exam }
+    conversion[self.condition]  
   end
   
+  # Get the test state
+  # Some tests have multiple states, like study/recall.
+  # Study, Recall, Finished
   def get_state
-    if self.state == "study"
-      state = :study
-    elsif self.state == "recall"
-      state = :recall
-    else
-      state = :finished
-    end
-    state
+    conversion = { "study" => :study, "recall" => :recall, "finished" => :finished }
+    conversion[self.state]
   end
   
+  # Initialized unanswered question list
+  # CSV string of all question IDs that have not been answered
   def init_unanswered
     self.unanswered = Question.where( :active => true ).map{ |q| q.id }.join( "," )
   end
   
+  # Get unanswered question ids
   def unanswered_arr
     self.unanswered.blank? ? Array.new : self.unanswered.split( "," )
   end
   
+  # Initialize question queue
+  # Set up a random array of all unanswered questions, CSV string
   def init_queue
     unanswered_arr = self.unanswered_arr
     
@@ -63,6 +64,7 @@ class Test < ActiveRecord::Base
     self.queue = unanswered_arr.join( "," )
   end
   
+  # Get next question from the question queue
   def get_next_question_from_queue
     queue_arr = self.queue.split( "," )
     
@@ -72,26 +74,32 @@ class Test < ActiveRecord::Base
     end
   end
   
+  # Remove the current question from the question queue
   def remove_current_question_from_queue
     queue_arr = self.queue.split( "," )
     queue_arr.delete_at(0) if queue_arr.size > 0
     self.queue = queue_arr.join( "," )
   end
   
+  # Length of the question queue
   def queue_size
     self.queue.blank? ? 0 : self.queue.split( "," ).size
   end
   
+  # Length of the list of unanswered questions
   def unanswered_size
     self.unanswered.blank? ? 0 : self.unanswered.split( "," ).size
   end
   
+  # Remove a question from the unanswered list
   def remove_question_from_unanswered( question )
     unanswered_arr = self.unanswered_arr
     unanswered_arr.delete( question.id.to_s )
     self.unanswered = unanswered_arr.join( "," )
   end
   
+  # Clear All Test Info
+  # Reset test to its original unset state, delete responses  
   def clear_all
     Response.where( :test_id => self.id ).each do |response|
       response.delete
