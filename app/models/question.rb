db901d22ae331f551576fc8471de97f15557d1c3
Zach Dicklin <zdicklin@gmail.com>
2012-01-20 16:57:21 -0500

    Reenable spell checking

--- app/models/question.rb
+++ app/models/question.rb
@@ -67,10 +67,9 @@ class Question < ActiveRecord::Base
   
   # Check Spelling
   def self.check_spelling( word )
-    #@@spell_checker = SpellingBee.new :source_text => "./public/wordlist.txt" if @@spell_checker == nil  
-    #corrections = @@spell_checker.correct word    
-    #( corrections.length > 0 and corrections[0] != word ) ? corrections[0] : nil    
-    nil
+    @@spell_checker = SpellingBee.new :source_text => "./public/wordlist.txt" if @@spell_checker == nil  
+    corrections = @@spell_checker.correct word    
+    ( corrections.length > 0 and corrections[0] != word ) ? corrections[0] : nil    
   end
   
 end
